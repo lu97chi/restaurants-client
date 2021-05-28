9a3f9a242a1738584d00b2ccf643f271de537e03
Hernandez <lu97is@gmail.com>
2021-05-28 16:16:46 -0500

    only one test

--- src/components/show-item/show-item.spec.js
+++ src/components/show-item/show-item.spec.js
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import ShowItem from "./index";
 import { services } from "../showcase/constants";
 
 test("Show Item", () => {
   const { title, subtitle, icon } = services[0];
-  render(<ShowItem title={title} subtitle={subtitle} icon={icon} />);
+  render(
+    <MemoryRouter>
+      <ShowItem title={title} subtitle={subtitle} icon={icon} />
+    </MemoryRouter>
+  );
   const titleEl = screen.getByText(title);
   const subtitleEl = screen.getByText(subtitle);
 
